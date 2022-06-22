482b3e68397582d01697abc020f6e3b6788ebc22
andresathler <andremoriya.sathler@gmail.com>
2022-06-22 14:16:48 -0300

    commit heroku

--- src/staff/entities/staff.entity.ts
+++ src/staff/entities/staff.entity.ts
@@ -1,8 +1,11 @@
+import { User } from '../../user/entities/user.entity';
+
 export class Staff {
-  userStaffId?: string;
+  userStaffId: string;
   whatsapp: string;
   description: string;
   minConvidados: number;
   maxConvidados: number;
   tipoBuffet: string;
+  user?: User;
 }
